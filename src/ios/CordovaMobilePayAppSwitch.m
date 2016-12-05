a2ee6ea27277975cfb2fcc3582335d53d3fcf35a
Mads Kessel <mads@all2day.dk>
2016-12-05 14:30:41 +0100

    random ...

--- src/ios/CordovaMobilePayAppSwitch.m
+++ src/ios/CordovaMobilePayAppSwitch.m
@@ -20,14 +20,18 @@ NSString *myCallbackId;
 - (void)startPayment:(CDVInvokedUrlCommand *)command {
     NSString* urlScheme = [self.commandDelegate.settings objectForKey:[@"urlScheme" lowercaseString]];
     NSString* merchantId = [self.commandDelegate.settings objectForKey:[@"merchantId" lowercaseString]];
+    printf("start");
     NSLog(@"2startPayment, urlScheme: '%@', merchantId: '%@''", urlScheme, merchantId);
+    printf("addObserver");
   //[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleOpenURL:) name:urlScheme object:nil];
     NSLog(@"After addObserver: country:'%i'",MobilePayCountry_Denmark);
-
+    printf("after addObserver");
   //[[MobilePayManager sharedInstance] setupWithMerchantId:merchantId merchantUrlScheme:urlScheme country:MobilePayCountry_Denmark];
+    printf("after setup");
     NSLog(@"After setupWithMerchantId");
     NSLog(@"command:'%@'",command);
 
+    printf("fetching values");
     myCallbackId = command.callbackId;
     NSString* amountStr = [command.arguments objectAtIndex:0];
     NSString* orderId = [command.arguments objectAtIndex:1];
